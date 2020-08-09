b0520242e5193d40321642c56f1db7340e53ade9
vimal kovath <vimal.k@fulfillsolutions.com>
2020-08-09 07:51:49 +0530

    updated

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -12,7 +12,7 @@ import { Chart } from "chart.js";
 })
 export class ProfilePage implements OnInit {
 
-  @ViewChild('lineCanvas', {static: false}) lineCanvas: ElementRef;
+  @ViewChild('lineCanvas', {static: true}) lineCanvas: ElementRef;
 
 
   items: Array<any>;
